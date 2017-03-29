ba2d2e735b5b9ae11c0272f75a0067e260ded41a
Ciobanu, Andrei-Nicolin (UK - EDC) <andreinicolinciobanu@deloitte.co.uk>
2017-03-29 22:14:27 +0300

    initial commit

---
+++ src/main/java/net/andreinc/aleph/AlephFormatter.java
@@ -0,0 +1,7 @@
+package net.andreinc.aleph;
+
+/**
+ * Created by andreinicolinciobanu on 29/03/17.
+ */
+public class AlephFormatter {
+}
---
+++ src/test/java/net/andreinc/aleph/AlephFormatterTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by andreinicolinciobanu on 29/03/17.
+ */
+public class AlephFormatterTest {
+
+}
